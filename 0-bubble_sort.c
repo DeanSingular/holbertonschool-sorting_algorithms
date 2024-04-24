5d4c6db9ea37431fb6b74be2420845e7688e4af8
DeanSingular <DMabuela@singular.co.za>
2024-04-24 06:21:17 +0000

    holbertonschool-sorting_algorithms

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,26 +1,29 @@
 #include "sort.h"
 
 /**
- * bubble_sort - Calls function
- * @array: Array to be sorted
- * @size: Size of array
- * Description: Function that sorts an array using the bubble sort method
- * Return: 0
+ * bubble_sort - Sorts an array of integers in ascending order
+ * using the Bubble sort algorithm
+ *
+ * @array: The array to be sorted
+ * @size: Number of elements in @array
  */
 void bubble_sort(int *array, size_t size)
 {
-	unsigned int i, j;
+	size_t i, j;
+	int tmp;
 
-	if (!array)
+	if (array == NULL || size < 2)
 		return;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size - 1; ++i)
 	{
-		for (j = 0; j < size - i - 1; j++)
+		for (j = 0; j < size - i - 1; ++j)
 		{
 			if (array[j] > array[j + 1])
 			{
-				swap(&array[j], &array[j + 1]);
+				tmp = array[j + 1];
+				array[j + 1] = array[j];
+				array[j] = tmp;
 				print_array(array, size);
 			}
 		}
